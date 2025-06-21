843b16056cec94f67afe78f748f31d6687fe9ae7
sanjayrabadiya <sanjayrabadiya30@gmail.com>
2025-06-21 19:33:40 +0530

    Done home page

--- js/custom.js
+++ js/custom.js
@@ -32,37 +32,111 @@ $(document).ready(function () {
 	$('.btn-close-sidebar,.sidebar-menu ul li a').click(function (event) {
 		$('body').removeClass('showSidebar');
 	});
-	if ($('.gallery-slider').length > 0) {
-		$('.gallery-slider').slick({
-			speed: 5000,
+
+	$('.custom-tab').click(function(){  
+      $(".tab").removeClass('tab-active');
+      $(".tab[data-id='"+$(this).attr('data-id')+"']").addClass("tab-active");
+      $(".custom-tab").removeClass('active');
+      $(this).parent().find(".custom-tab").addClass('active');
+    });
+
+	if ($('.home-travel-slider').length > 0) {
+		$('.home-travel-slider').slick({
+			speed: 1000,
 			autoplay: true,
-			autoplaySpeed: 0,
-			centerMode: true,
-			cssEase: 'linear',
-			slidesToShow: 4,
+			slidesToShow: 5,
 			draggable:false,
 			focusOnSelect:false,
-			pauseOnFocus:false,
-			pauseOnHover:false,
 			slidesToScroll: 1,
-			infinite: true,
-			initialSlide: 1,
 			arrows: false,
 			buttons: false,
 			responsive: [
 				{
 				  breakpoint: 767,
+				 settings: "unslick"
+				},
+			]
+		});
+	}
+	if ($('.client-logo-list').length > 0) {
+		$('.client-logo-list').slick({
+			speed: 1000,
+			autoplay: true,
+			slidesToShow: 7,
+			draggable:false,
+			slidesToScroll: 1,
+			variableWidth: true,
+			arrows: false,
+			buttons: false,
+			responsive: [
+				 {
+					breakpoint: 9999,
+					settings: "unslick"
+				},
+				{
+				  breakpoint: 1600,
+				  settings: {
+					slidesToShow: 6,
+				  }
+				},
+				{
+				  breakpoint: 1400,
+				  settings: {
+					slidesToShow: 5,
+				  }
+				},
+				{
+				  breakpoint: 1200,
+				  settings: {
+					slidesToShow: 4,
+				  }
+				},
+				{
+				  breakpoint: 991,
 				  settings: {
 					slidesToShow: 3,
-					speed: 3000,
+				  }
+				},
+				{
+				  breakpoint: 767,
+				  settings: {
+					slidesToShow: 2,
 				  }
 				},
 				{
 				  breakpoint: 575,
 				  settings: {
 					slidesToShow: 2,
-					speed: 3000,
-					
+				  }
+				}
+			]
+		});
+	}
+	if ($('.instagram-slider').length > 0) {
+		$('.instagram-slider').slick({
+			speed: 1000,
+			//autoplay: true,
+			slidesToShow: 7,
+			slidesToScroll: 1,
+			arrows: false,
+			buttons: false,
+			responsive: [
+				{
+				  breakpoint: 1600,
+				  settings: {
+					slidesToShow: 6,
+				  }
+				},
+				{
+				  breakpoint: 1400,
+				  settings: {
+					slidesToShow: 5,
+				  }
+				},
+				{
+				  breakpoint: 1200,
+				  settings: {
+					slidesToShow: 4,
 				  }
 				}
 			]
